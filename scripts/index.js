11e63a3ebbd5b3f8d255087b2f259b71156e08e5
susan850926 <55375620+susan850926@users.noreply.github.com>
2019-12-28 22:12:34 +0800

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm'), // Required
+    path: 'https://assets7.lottiefiles.com/packages/lf20_CqpXvC.json', // Required
+    renderer: 'svg', // Required
+    loop: true, // Optional
+    autoplay: true, // Optional
+    name: 'data.json', // Name for future reference. Optional.
+  })
